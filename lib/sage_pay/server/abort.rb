7c17b28144cef1b9a4d737c7cd9f3a50de7c8803
Zakir <zakirsajib@yahoo.co.uk>
2011-12-09 11:18:23 +0000

    comment out

--- lib/sage_pay/server/abort.rb
+++ lib/sage_pay/server/abort.rb
@@ -5,10 +5,10 @@ module SagePay
 
       attr_accessor :vps_tx_id, :security_key, :tx_auth_no
 
-      validates_presence_of :vps_tx_id, :security_key, :tx_auth_no
+      #validates_presence_of :vps_tx_id, :security_key, :tx_auth_no
 
-      validates_length_of :vps_tx_id,    :is => 38
-      validates_length_of :security_key, :is => 10
+      #validates_length_of :vps_tx_id,    :is => 38
+      #validates_length_of :security_key, :is => 10
 
       def post_params
         super.merge({
